f39a4c0c8132c29521b6a12aefd194fd1ce0cf49
inga Bacher <ingalila@hotmail.fr>
2019-07-17 16:52:28 +0200

    updated menu

---
+++ api/menu.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = new express.Router();
+const Article = require("../models/Menu");
+
+const getAll = () => Menu.find();
+const getOne = id => Menu.findById(id);
+const updateOne = (id, data) => Menu.findByIdAndUpdate(id, data);
+const deleteOne = id => Menu.findByIdAndDelete(id);
+const create = data => Menu.create(data);
+
+router.get("/", (req, res) => {
+  getAll()
+    .then(menus => {
+      res.status(200).send(menus);
+    })
+    .catch(error => res.status(500).send("Something went wrong"));
+});
+
+router.get("/:id", (req, res) => {
+  getOne(req.params.id)
+    .then(menu => {
+      res.status(200).send(menu);
+    })
+    .catch(error => res.status(500).send("Something went wrong"));
+});
+
+router.post("/", (req, res) => {
+  create(req.body)
+    .then(menu => res.status(200).send(menu))
+    .catch(err => res.status(500).send("Something went wrong"));
+});
+
+router.delete("/:id", (req, res) => {
+  deleteOne(req.params.id)
+    .then(menus => {
+      res.status(200).send(menus);
+    })
+    .catch(error => res.status(500).send("Something went wrong"));
+});
+
+router.patch("/:id", (req, res) => {
+  updateOne(req.params.id).then(updatedDocument =>
+    res.status(200).send(updatedDocument)
+  );
+});
+
+module.exports = {
+  router,
+  getAll,
+  getOne,
+  deleteOne,
+  updateOne,
+  create
+};
--- models/Menu.js
+++ models/Menu.js
@@ -1,1 +1,13 @@
-const 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const menuSchema = new Schema({
+  title: String,
+  Image: [{ type: String }],
+  description: String,
+  ingredients: String
+});
+
+const menuModel = mongoose.model("Menu", menuSchema);
+
+module.exports = menuModel;
