d04c93d323ba2d6e8335444853a2c8e5ef8237c6
Elise <elisegamassa@gmail.com>
2019-07-18 16:17:02 +0200

    routes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const cors = require("cors");
 
 const corsOptions = { origin: process.env.FRONTEND_URL };
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
@@ -36,8 +36,12 @@ app.use("/auth", authRoute);
 
 const userAPI = require("./api/user");
 const articleAPI = require("./api/menu");
+const alimentAPI = require("./api/aliment");
+const commandAPI = require("./api/command");
 app.use("/api/users", userAPI.router);
 app.use("/api/menu", articleAPI.router);
+app.use("/api/aliments", alimentAPI.router);
+app.use("api/command", commandAPI.router);
 
 app.listen(process.env.PORT, () => {
   console.log("App hosted on: ", process.env.SITE_URL);
