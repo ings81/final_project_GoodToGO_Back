57ecef3a321e45b908d4187f5f004c6de06c49a9
inga Bacher <ingalila@hotmail.fr>
2019-07-18 14:26:01 +0200

    updated user +menu

--- models/Menu.js
+++ models/Menu.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const menuSchema = new Schema({
-  title: String,
+  Title: String,
   Image: [{ type: String }],
-  ingredients: String,
-  description: String
+  Ingredients: [{ type: String }],
+  Description: String
 });
 
 const menuModel = mongoose.model("Menu", menuSchema);
--- models/User.js
+++ models/User.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstname: String,
-  lastname: String,
-  email: String,
-  password: String
+  FirstName: String,
+  LastName: String,
+  Email: String,
+  Password: String
 });
 
 const userModel = mongoose.model("User", userSchema);
